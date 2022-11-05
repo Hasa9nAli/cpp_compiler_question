c829c40734dc40741b428fe0a6a7e21b470782f4
Hasan Ali <92353024+Hasa9nAli@users.noreply.github.com>
2022-11-06 00:18:14 +0300

    Create identifier_exersice_with_algorithm.cpp

---
+++ identifier_exersice_with_algorithm.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std; 
+void _error_message(string type_error , char letter= NULL) {
+	cout << "Error Becase " << type_error<<" " << letter << endl;
+}
+void main() {
+	char letter; 
+	int state = 1; 
+	while (state == 1 || state == 2) {
+		switch (state) {
+		case 1: {
+			cin >> letter;
+			if (isalpha(letter) || letter == '_')
+				state = 2;
+			else {
+				_error_message("Not start with letter or _ but start with ", letter);
+				state = 999;
+			}
+		};
+			  break; 
+		case 2: {
+			cin >> letter; 
+			if (isalpha(letter) || isdigit(letter) || letter == '_')
+				state = 2;
+			else
+				state = 3; 
+		}
+		}
+	}
+	if (state == 3)
+		_error_message("there is error in name of identifier " , letter ); 
+}
