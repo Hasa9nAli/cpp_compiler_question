c7c036be438b979bd4cab853a9475dc600c4b3ba
Hasan Ali <92353024+Hasa9nAli@users.noreply.github.com>
2023-10-19 17:18:20 +0300

    Create identifier_finite_state.cpp

---
+++ identifier_finite_state/identifier_finite_state.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+bool isLetter(char character){
+        if((!(character >= 'A' && character <= 'Z')) && (character >= 'a' && character <='z'))
+            return true ;
+        return false;
+    }
+bool isDigit(char character){
+    if( character >= '0' && character <= '9')
+        return true ;
+
+    return false;
+}
+
+int main() {
+    char character;
+    const int FIRST_STATE = 1 ;
+    const int SECOND_STATE = 2;
+    const int THIRD_STATE = 3;
+    int state = FIRST_STATE;
+    cout<< "Enter Your Character";
+    cin>> character;
+    if(isLetter((character))) {
+        cin >> character;
+        state = SECOND_STATE;
+
+        while (isLetter(character) || isDigit(character)) {
+            cin >> character;
+            state = SECOND_STATE;
+        }
+        if (state == THIRD_STATE) {
+            cout << "ACCEPTED";
+        } else
+            cout << "ERROR CASE";
+    }
+    else cout<< "Error Not Found Letter in start " ;
+    return 0;
+}
